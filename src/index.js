c23418a7eb6eb8a0487ce7723226b279b92b075f
MifDX <104031797+MifDX@users.noreply.github.com>
2023-01-28 01:08:09 +0300

    fix: fix arrors

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === (undefined)) { 
+    return [];} 
+    else {
+    return matrix.reduce((accmulator, curient, i) => {
+      curient.sort((a, b) => !(i & 1) ? a - b : b - a).map(e => accmulator.push(e));
+  return accmulator;
+}, []);}
 }
